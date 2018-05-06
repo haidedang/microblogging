23f1821dda3b5584d218d02317a8949cf48f215a
Hai Dang <haiduc.dang91@gmail.com>
2018-05-06 14:07:58 +0200

    Project Setup

---
+++ server/src/server.js
@@ -0,0 +1,45 @@
+const express = require('express'); 
+const path = require('path'); 
+const MetaAuth = require('meta-auth'); 
+
+const app = express(); 
+const bodyParser = require('body-parser'); 
+const metaAuth = new MetaAuth(); 
+
+let address; 
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+app.get('/',(req, res)=> { 
+    res.sendFile(path.join(__dirname + '/index.html')); 
+})
+
+// Login Function 
+app.get('/login/:MetaAddress', metaAuth, (req, res) => {
+    // Request a message from the server
+    if (req.metaAuth && req.metaAuth.challenge) {
+      res.send(req.metaAuth.challenge)
+    }
+  });
+
+app.get('/auth/:MetaMessage/:MetaSignature', metaAuth, (req,res)=> { 
+    if(req.metaAuth && req.metaAuth.recovered){ 
+        res.send(req.metaAuth.recovered); 
+    } else { 
+        res.status(400);
+    }
+})
+
+app.listen(3000, ()=>{
+    console.log("Listening on 3000"); 
+})
